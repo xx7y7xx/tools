7b4f459a9e89023a45988fb979862204f48d297a
Devin Chenyang <devin.chenyang@grabtaxi.com>
2024-08-01 22:30:47 +0800

    chore: Add Sentry error monitoring and performance tracking

--- src/Application/index.tsx
+++ src/Application/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as Sentry from '@sentry/react';
 
 // import { initGa } from './init';
 import SearchTrain from './searchTrain';
@@ -6,6 +7,24 @@ import Setting from './Setting';
 
 // Styles for application
 import './index.css';
+
+Sentry.init({
+  dsn: 'https://f2bd9c0de1e6390cad39e357298557bd@o4507702515662848.ingest.us.sentry.io/4507702518874112',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.browserProfilingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', /^https:\/\/googleapis\.com\/api/],
+  // Profiling
+  profilesSampleRate: 1.0, // Profile 100% of the transactions. This value is relative to tracesSampleRate
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 export default function Application() {
   const urlParams = new URLSearchParams(window.location.search);
