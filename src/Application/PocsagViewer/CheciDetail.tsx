10016f730da6eaf843807710a21d9b849ad814a4
Devin Chenyang <devin.chenyang@grabtaxi.com>
2025-04-12 16:08:53 +0800

    train speed slider in a separate file

--- src/Application/PocsagViewer/CheciDetail.tsx
+++ src/Application/PocsagViewer/CheciDetail.tsx
@@ -1,11 +1,7 @@
-import { useEffect, useState } from 'react';
-
-import { Button, Slider } from 'antd';
-
 import { TrainSignalRecord } from './types';
 import MileageChart from './MileageChart';
 import SpeedChart from './SpeedChart';
-
+import TrainSlider from './TrainSlider';
 /**
  * Show the detail of a train's checi
  * Show an animation of the handler on slider
@@ -15,68 +11,9 @@ const CheciDetail = ({
 }: {
   trainSignalRecords: TrainSignalRecord[];
 }) => {
-  const [arrIndexValue, setArrIndexValue] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  // Find the min and max mileage values
-  const minMileage = Math.min(
-    ...trainSignalRecords.map((record) => record.payload.mileage)
-  );
-  const maxMileage = Math.max(
-    ...trainSignalRecords.map((record) => record.payload.mileage)
-  );
-
-  useEffect(() => {
-    let interval: NodeJS.Timeout;
-
-    if (isPlaying) {
-      interval = setInterval(() => {
-        setArrIndexValue((prevValue) => {
-          const nextValue = prevValue + 1;
-          if (nextValue > trainSignalRecords.length - 1) {
-            setIsPlaying(false);
-            return prevValue;
-          }
-          return nextValue;
-        });
-      }, 500);
-    }
-
-    return () => {
-      if (interval) {
-        clearInterval(interval);
-      }
-    };
-  }, [isPlaying, trainSignalRecords.length]);
-
-  const handlePlay = () => {
-    setArrIndexValue(0);
-    setIsPlaying(true);
-  };
-
-  const handlePause = () => {
-    setIsPlaying(false);
-  };
-
-  const handleSlideChange = (value: number) => {
-    setArrIndexValue(value);
-  };
-
   return (
     <div>
-      <Slider
-        value={trainSignalRecords[arrIndexValue].payload.mileage}
-        onChange={handleSlideChange}
-        min={minMileage}
-        max={maxMileage}
-        step={0.1}
-        tooltip={{
-          open: true,
-          formatter: (value) => `${value} km`,
-        }}
-      />
-      <Button onClick={handlePlay}>Play</Button>
-      <Button onClick={handlePause}>Pause</Button>
+      <TrainSlider trainSignalRecords={trainSignalRecords} />
       <MileageChart trainSignalRecords={trainSignalRecords} />
       <SpeedChart trainSignalRecords={trainSignalRecords} />
     </div>
