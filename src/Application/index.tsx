ea7179af68a89c315875f39311f20d11c6f85f08
Devin Chenyang <devin.chenyang@grabtaxi.com>
2025-04-16 22:30:49 +0800

    sentry disable mask

--- src/Application/index.tsx
+++ src/Application/index.tsx
@@ -22,7 +22,11 @@ Sentry.init({
      */
     // Sentry.browserTracingIntegration(),
     // Sentry.browserProfilingIntegration(),
-    Sentry.replayIntegration(),
+    Sentry.replayIntegration({
+      // NOTE: This will disable built-in masking. Only use this if your site has no sensitive data, or if you've already set up other options for masking or blocking relevant data, such as 'ignore', 'block', 'mask' and 'maskFn'.
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
   ],
   // Performance Monitoring
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
