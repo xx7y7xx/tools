e0d5d87ddf49e0270b468217fe37766cceedbaf8
Devin Chenyang <devin.chenyang@grabtaxi.com>
2025-04-18 23:59:41 +0800

    revert src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import PocsagViewer from './Application/PocsagSignalViewer';
+import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <PocsagViewer />
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
